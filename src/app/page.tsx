f1e94606af841fded6032316367f83470e3d14c5
Amir-Adel9 <amiradel7152@gmail.com>
2023-06-20 21:19:54 +0300

    debugging (latest)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,41 +21,41 @@ dayjs.extend(utcPlugin);
 dayjs.extend(durationPlugin);
 
 export default async function Home() {
-  // const loggedInUser = await currentUser();
+  const loggedInUser = await currentUser();
 
-  // if (loggedInUser) {
-  //   const { id, username } = loggedInUser;
-  //   console.log(id, username);
+  if (loggedInUser) {
+    const { id, username } = loggedInUser;
+    console.log(id, username);
 
-  //   const existingUser = await db
-  //     .select()
-  //     .from(user)
-  //     .where(eq(user.clerkId, id));
-  //   console.log('existingUser', existingUser);
+    const existingUser = await db
+      .select()
+      .from(user)
+      .where(eq(user.clerkId, id));
+    console.log('existingUser', existingUser);
 
-  //   if (existingUser.length === 0) {
-  //     const newUser = await db
-  //       .insert(user)
-  //       .values({ clerkId: id, username: username });
-  //     console.log('newUser', newUser);
-  //   }
-  // }
+    if (existingUser.length === 0) {
+      const newUser = await db
+        .insert(user)
+        .values({ clerkId: id, username: username });
+      console.log('newUser', newUser);
+    }
+  }
 
-  // const upcomingMatches = await fetch(
-  //   `https://esports-api.lolesports.com/persisted/gw/getSchedule?hl=en-US&leagueId=${process.env.NEXT_PUBLIC_LEAGUE_ID}`,
-  //   requestParams
-  // )
-  //   .then((res) => res.json())
-  //   .then((upcomingMatches) => {
-  //     const unStartedMatchesWithin7Days = upcomingMatches.data.schedule.events
-  //       .filter((event: any) => {
-  //         const matchState = event.state;
+  const upcomingMatches = await fetch(
+    `https://esports-api.lolesports.com/persisted/gw/getSchedule?hl=en-US&leagueId=${process.env.NEXT_PUBLIC_LEAGUE_ID}`,
+    requestParams
+  )
+    .then((res) => res.json())
+    .then((upcomingMatches) => {
+      const unStartedMatchesWithin7Days = upcomingMatches.data.schedule.events
+        .filter((event: any) => {
+          const matchState = event.state;
 
-  //         return matchState === 'unstarted' || matchState === 'inProgress';
-  //       })
-  //       .slice(0, 8);
-  //     return unStartedMatchesWithin7Days;
-  //   });
+          return matchState === 'unstarted' || matchState === 'inProgress';
+        })
+        .slice(0, 8);
+      return unStartedMatchesWithin7Days;
+    });
 
   return (
     <main className='relative flex min-h-screen flex-col items-center'>
