1c0c7fd83c5346caf680f82d24fd318d1904958a
Amir-Adel9 <amiradel7152@gmail.com>
2023-06-20 20:47:18 +0300

    production crash fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,6 @@ import { db } from '@/db';
 import { prediction, user } from '@/db/schema';
 import { eq, sql } from 'drizzle-orm';
 import { requestParams } from '@/utils/requestParams';
-import { revalidatePath } from 'next/cache';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
@@ -29,7 +28,6 @@ export const metadata = {
 };
 
 async function fulfillPredictions() {
-  'use server';
   const pendingPredictions = await db
     .select()
     .from(prediction)
@@ -92,7 +90,6 @@ async function fulfillPredictions() {
         }
       });
   });
-  revalidatePath(`/leaderboard`);
 }
 
 fulfillPredictions();
