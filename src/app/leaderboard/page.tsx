a8d56f42c15a8ba5ed7cd36d4686fbe7ea8e242e
Amir-Adel9 <amiradel7152@gmail.com>
2023-07-04 23:37:27 +0300

    added leaderboard maintenance heads up

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -232,7 +232,7 @@ async function LeaderBoard() {
             Due to an occurring maintenance the points may not update right
             away, check back soon.
           </h1> */}
-          <table className='w-full text-sm text-left'>
+          {/* <table className='w-full text-sm text-left'>
             <thead className='text-xs  uppercase bg-gray-50 '>
               <tr>
                 <th scope='col' className='px-6 py-3'>
@@ -266,7 +266,23 @@ async function LeaderBoard() {
                 }
               )}
             </tbody>
-          </table>
+          </table> */}
+          <div className='relative z-10 flex flex-col items-center justify-center'>
+            <Image
+              src='/dinger.gif'
+              alt='dinger Image'
+              width={260}
+              height={260}
+              draggable={false}
+            />
+            <h1 className='text-5xl font-bold text-accent-gold text-center'>
+              The leaderboard is currently under maintenance
+            </h1>
+            <p className='text-2xl text-accent-gold text-center'>
+              Please check back later
+            </p>
+            <p className='text-accent-gold text-center'></p>
+          </div>
         </div>
         <div className='w-full lg:w-[30%] h-screen bg-secondary text-primary shadow-2xl overflow-y-auto no-scrollbar'>
           <h1 className='text-center font-bold text-2xl p-5'>
