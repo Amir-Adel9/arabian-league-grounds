f3a4ff04234a674d956ab9937178a1033cec14d0
Amir-Adel9 <amiradel7152@gmail.com>
2023-07-19 10:17:26 +0300

    Loading states

---
+++ src/app/standings/loading.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+function error() {
+  return (
+    <main className='relative flex min-h-screen flex-col items-center'>
+      <section className='w-full min-h-screen relative flex flex-col justify-center items-center'>
+        <div className='relative z-10 flex flex-col items-center justify-center'>
+          <Image
+            src='/dinger.gif'
+            alt='dinger Image'
+            width={260}
+            height={260}
+            draggable={false}
+          />
+          <h1 className='text-5xl font-bold text-accent-gold text-center'>
+            Loading...
+          </h1>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default error;
